6123df2976da9a82f43005e0ce1cb122d4598d4a
Colin Worf and Lena Ha <colin+lena@>
2018-01-10 13:16:36 -0800

    create function for output of api data

--- dev/js/template-logic.js
+++ dev/js/template-logic.js
@@ -1,17 +1,22 @@
-export function findLaunch(filterBy, filterValue){
+export function findLaunch(filterBy, filterValue, renderCallback){
   const xhr = $.get(`https://api.spacexdata.com/v2/launches?${filterBy}=${filterValue}`
 );
   xhr.done(function(response) {
-    console.log("We got the data!", response);
+    // console.log("We got the data!", response);
 
     var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
     var match = response[0].links.video_link.match(regExp);
-
+    console.log(match);
+    let id;
     if (match && match[2].length == 11) {
-        return match[2];
+        id = match[2];
     } else {
-        return 'error';
+        id = 'error';
     }
 
+    renderCallback(id);
   });
+  xhr.fail(function() {
+    console.log("FAILURE");
+  })
 }
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -6,8 +6,10 @@ $(document).ready(function(){
     // let date = Date.now();
     const filterBy = $("#filter-by").val();
     const filterValue = $("#filter-value").val();
-    let myId = findLaunch(filterBy, filterValue);
+    findLaunch(filterBy, filterValue, render);
 
-    $("#results").html(`<iframe width="560" height="315" src="//www.youtube.com/embed/${myId}" frameborder="0" allowfullscreen></iframe>`);
   });
+  function render(myId){
+    $("#results").html(`<iframe width="560" height="315" src="//www.youtube.com/embed/${myId}" frameborder="0" allowfullscreen></iframe>`);
+  }
 });
