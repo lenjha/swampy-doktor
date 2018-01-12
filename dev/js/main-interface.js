775ca533fb717bc458afd37dbe26b40dafce6f55
Guest User <Guest@Epicodus-3C.local>
2018-01-12 09:21:11 -0800

    framework, sign up for apiKey

--- dev/js/main-interface.js
+++ dev/js/main-interface.js
@@ -1,51 +1,5 @@
-import { findLaunch } from "./../dev/js/main-logic.js";
+import { findDoctor } from "./../dev/js/main-logic.js";
 
 $(document).ready(function(){
 
-// FUNCTION DEFINITIONS
-  function render(myIds, flightNumbers, dates, payloads, temperatures){
-    $("#results").text(''); //clear the section
-
-    for (var i = 0; i < flightNumbers.length; i++) {
-      let myId = myIds[i];
-      let flightNumber = flightNumbers[i];
-      let payload = payloads[i];
-      let temperature = temperatures[i];
-      let template = `<div class="item">
-      <h2>launch ${flightNumber}</h2>
-      <div class="meta"><span class="meta-title">Payload: </span>${payload}</div>
-      <div class="meta"><span class="meta-title">Temperature: </span>${temperature}</div>
-      <iframe width="560" height="315" src="//www.youtube.com/embed/${myId}" frameborder="0" allowfullscreen></iframe>
-      </div>`
-
-      $("#results").append(template); //output the section
-    }
-
-  }
-  // function weatherReport(temperature){
-  //   $('#weather-area').text(`The temperature in ${} is ${temperature} degrees`)
-  // }
-  // weatherApi(weatherReport);
-
-  // CLICK FUNCTION
-  $("#submit-form").click(function(event){
-    event.preventDefault();
-    // let date = Date.now();
-    let filterBy = [];
-    let filterValue = [];
-
-    $('*[name=filter]').each(function(){
-      if ($(this).val()) {
-        filterBy.push($(this).attr('id'));
-        filterValue.push($(this).val());
-      }
-    });
-    console.log(filterValue);
-    if (filterValue.length != 0) {
-      findLaunch(filterBy, filterValue, render);
-    } else {
-      alert("pick a filter yo!");
-    }
-
-  });
 });
--- dev/js/main-logic.js
+++ dev/js/main-logic.js
@@ -1,48 +1,9 @@
-import { weatherApi } from "./weather-logic.js";
+// import {  } from "./weather-logic.js";
 
-export function findLaunch(filterBy, filterValue, render){
-  let filters = [];
-  for (var i = 0; i < filterBy.length; i++) {
-    let by = filterBy[i];
-    let val = filterValue[i];
-    filters.push(`${by}=${val}`);
+export function findDoctor(location){
+  var apiKey = require('./../../.env').apiKey;
 
-  }
+  const xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&skip=0&limit=10&user_key=${apiKey}`)
+  // const xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&user_key=${apiKey}`)
 
-  filters = filters.join("&");
-
-  const xhr = $.get(`https://api.spacexdata.com/v2/launches/?${filters}`);
-
-  xhr.done(function(responses) {
-
-    const myIds = [];
-    const flightNumbers = [];
-    const payloads = [];
-    const dates = [];
-
-
-    responses.forEach(function(response){
-
-      flightNumbers.push(response.flight_number);
-      payloads.push(response.payload);
-      dates.push(response.launch_date_local);
-
-      var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-      var match = response.links.video_link.match(regExp);
-
-      let id;
-      if (match && match[2].length == 11) {
-        myIds.push(match[2]);
-
-      } else {
-        console.log("FAILURE to quantify");
-      }
-    });
-
-    weatherApi(myIds, flightNumbers, dates, payloads, render)
-    // render(myIds, flightNumbers, weather);
-  });
-  xhr.fail(function() {
-    console.log("FAILURE to reprogram the link to lowLetter");
-  });
 }
