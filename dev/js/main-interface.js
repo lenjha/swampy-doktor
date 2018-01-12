7e554346b8182eede8ef36b4d41e08e2804e1d88
Guest User <Guest@Epicodus-3C.local>
2018-01-12 11:57:56 -0800

    use api-fetched array items to smack items into #results via looping

--- dev/js/main-interface.js
+++ dev/js/main-interface.js
@@ -2,7 +2,25 @@ import { findDoctor } from "./../dev/js/main-logic.js";
 
 $(document).ready(function(){
 
+  function render(names, addresses, phoneNumbers, websites, availabilities){
+    // clear the field
+    $("#results").text(`Test.`);
+    for (var i = 0; i < doctors.length; i++) {
+      let name = names[i];
+      let address = addresses[i];
+      let phoneNumber = phoneNumbers[i];
+      let website = websites[i];
+      let availability = availabilities[i];
+      let results = `<div class="result-item">
+        <h3>${name}</h3>
+        <p>${address}</p>
+        <p>${phoneNumber}</p>
+        <p>${website}</p>
+        <p>${availability}</p>
+      </div>`
 
+      ${"#results"}
+    }
 
   // CLICK!
   $("#submit-form").click(function(event){
--- dev/js/main-logic.js
+++ dev/js/main-logic.js
@@ -3,8 +3,8 @@ var apiKey = require('./../../.env').apiKey;
 
 export function findDoctor(symptoms){
 
-  const xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=10&user_key=${apiKey}`)
-  // const xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&skip=0&limit=10&user_key=${apiKey}`)
+  // const xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=10&user_key=${apiKey}`)
+  const xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&skip=0&limit=10&user_key=${apiKey}`)
   // const xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&user_key=${apiKey}`)
 
   xhr.done(function(responses){
