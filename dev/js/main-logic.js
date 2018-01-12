34ac02e4e334091ebfd5c70fba1dee35a34a3642
Guest User <Guest@Epicodus-3C.local>
2018-01-12 09:37:44 -0800

    set up some brainstormed variables

--- dev/js/main-logic.js
+++ dev/js/main-logic.js
@@ -6,4 +6,15 @@ export function findDoctor(location){
   const xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&skip=0&limit=10&user_key=${apiKey}`)
   // const xhr = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&user_key=${apiKey}`)
 
+  // return with doc info:
+      // VAR(response?).data.practices.name
+      // VAR(response?).data.practices.visit_address.street
+      // VAR(response?).data.practices.visit_address.street2
+      // VAR(response?).data.practices.visit_address.city
+      // VAR(response?).data.practices.visit_address.state
+      // VAR(response?).data.practices.visit_address.zip
+      // VAR(response?).data.practices.website
+      // VAR(response?).data.practices.phones.number
+      // VAR(response?).data.practices.accepts_new_patients
+
 }
